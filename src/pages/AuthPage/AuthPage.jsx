64dfe6aadd5a0d3d4c307c55119d97d8698b2ada
Aisha Ayinde <aishaayinde@Aishas-MacBook-Air.local>
2023-10-26 11:29:30 +0100

    MERN-Stack-Infrastructure

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,12 @@
+import SignUpForm from '../../components/SignUpForm/SignUpForm'
+import LoginForm from '../../components/LoginForm/LoginForm'
+
+export default function AuthPage({ setUser }) {
+    return (
+       <main>
+         <h1>Auth Page, welcome</h1>
+         <SignUpForm setUser={ setUser }/>
+         <LoginForm/>
+        </main>
+    )
+}
